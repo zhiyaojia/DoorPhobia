9fd4d159d2b11770d2a7232c2bc52c6f1e05a61c
wangtianrui2 <839775660@qq.com>
2021-09-26 00:36:50 -0700

    add word lock file
    add word lock file

---
+++ Assets/Scripts/Tianrui/WordLock.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordLock : MonoBehaviour
+{
+    public GameObject Wheel1;
+    public GameObject Wheel2;
+    public GameObject Wheel3;
+    public GameObject Button;
+    int CurrentChosenWheel = 0;
+    public string PasswordWord1;
+    public string PasswordWord2;
+    public string PasswordWord3;
+    string Wheel1Word = "";
+    string Wheel2Word = "";
+    string Wheel3Word = "";
+    int Wheel1Num = 0;
+    int Wheel2Num = 0;
+    int Wheel3Num = 0;
+    public GameObject Lock;
+    string[] PasswordDic = { "A", "B", "C", "D", "E", "F" };
+    float RotateDegree = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            CurrentChosenWheel--;
+            if (CurrentChosenWheel < 0) CurrentChosenWheel = 2;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            CurrentChosenWheel++;
+            if (CurrentChosenWheel > 2) CurrentChosenWheel = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            switch (CurrentChosenWheel)
+            {
+                case 0:
+                    LockRotate(Wheel1, true);
+                    Wheel1Num--;
+                    if (Wheel1Num < 0) Wheel1Num = 5;
+                    break;
+                case 1:
+                    LockRotate(Wheel2, true);
+                    Wheel2Num--;
+                    if (Wheel2Num < 0) Wheel2Num = 5;
+                    break;
+                case 2:
+                    LockRotate(Wheel3, true);
+                    Wheel3Num--;
+                    if (Wheel3Num < 0) Wheel3Num = 5;
+                    break;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            switch (CurrentChosenWheel)
+            {
+                case 0:
+                    LockRotate(Wheel1, false);
+                    Wheel1Num++;
+                    if (Wheel1Num > 5) Wheel1Num = 0;
+                    break;
+                case 1:
+                    LockRotate(Wheel2, false);
+                    Wheel2Num++;
+                    if (Wheel2Num > 5) Wheel2Num = 0;
+                    break;
+                case 2:
+                    LockRotate(Wheel3, false);
+                    Wheel3Num++;
+                    if (Wheel3Num > 5) Wheel3Num = 0;
+                    break;
+            }
+        }
+        if (ValidatePassword() == true)
+        {
+            Unlock();
+            Debug.Log("password correct!");
+        }
+    }
+    void LockRotate(GameObject Wheel, bool Direction)
+    {
+        if (Direction == false)
+        {
+            Wheel.transform.Rotate(-60, 0, 0);
+        }
+        else
+        {
+            Wheel.transform.Rotate(60, 0, 0);
+        }
+    }
+    bool ValidatePassword()
+    {
+        if (PasswordDic[Wheel1Num] == PasswordWord1 && PasswordDic[Wheel2Num] == PasswordWord2 && PasswordDic[Wheel3Num] == PasswordWord3)
+        {
+            return true;
+            Debug.Log("CurrentChosenWheel:" + CurrentChosenWheel);
+            Debug.Log("Correct");
+        }
+        return false;
+    }
+    void Unlock()
+    {
+        StartCoroutine("TranslateAndRotateMetal");
+    }
+    IEnumerator TranslateAndRotateMetal()
+    {
+        float RotateSpeed = -0.001f;
+        while (RotateDegree > -60)
+        {
+            Lock.transform.Rotate(0, 0, RotateSpeed);
+            RotateDegree += RotateSpeed;
+            yield return null;
+        }
+    }
+}
