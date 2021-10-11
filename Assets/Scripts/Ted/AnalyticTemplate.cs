f494c057a29b97b6c2ebebc32f8ac5876e71b624
Ted Xu <xujingto@usc.edu>
2021-10-10 22:05:27 -0700

    Update AnalyticTemplate.cs

--- Assets/Scripts/Ted/AnalyticTemplate.cs
+++ Assets/Scripts/Ted/AnalyticTemplate.cs
@@ -9,7 +9,9 @@
 //     //use for debug, test if analytic event works
 //     AnalyticsResult ar;
 //     float secondsElapsed = 0;
-//     // get the used time
+//     // get the used time, add to Control.cs
+//     private float startTime = 0;
+//     private float solveTime;
 //     private void Update()
 //     {
 //         // count the time used, need to be in Controller.cs file or the item would become unInteractable
@@ -18,7 +20,13 @@
 //     // some function you need to set up an event
 //     private void someFunction(){
 //         // set up a levelstart event
-//         AnalyticsEvent.LevelStart("diary_lock");
+//         AnalyticsEvent.LevelStart("3L_diary_lock");
+//         ar = AnalyticsEvent.LevelStart("3L_diary_lock");
+//         if (startTime == 0) 
+//         {
+//             startTime = diaryControl.secondsElapsed;
+//         }  
+//         Debug.Log("LCStart = " + ar.ToString() + startTime.ToString());
 //         //AnalyticsEvent.LevelStart("some word.....");
 //         // for more events, see 
 //         //https://docs.unity3d.com/cn/current/Manual/UnityAnalyticsStandardEvents.html
@@ -30,8 +38,24 @@
 //         Dictionary<string, object> customParams = new Dictionary<string, object>();
 //         customParams.Add("seconds_played", diaryControl.secondsElapsed);
 
-//         // set up a level complete event, same as level start        
-//         AnalyticsEvent.LevelComplete("diary_lock", customParams);
+//         // set up a level complete event, same as level start   
+//         solveTime = diaryControl.secondsElapsed - startTime;
+//         Dictionary<string, object> customParams = new Dictionary<string, object>();
+//         customParams.Add("seconds_played", solveTime.ToString());     
+
+//             // Finish event
+//             AnalyticsEvent.LevelComplete("3L_diary_lock", customParams);
+//             ar = AnalyticsEvent.LevelComplete("3L_diary_lock");
+//             Debug.Log("LCFinish = " + ar.ToString() + diaryControl.secondsElapsed.ToString() + "SolveTime=" + solveTime.ToString());
+//             // report custom event
+//             ReportSolve3LDiaryLock(solveTime);
+
+//             // Quit event
+//             AnalyticsEvent.LevelQuit("diary_lock", customParams);
+//             ar = AnalyticsEvent.LevelQuit("diary_lock");
+//             Debug.Log("LQResult = " + ar.ToString() + diaryControl.secondsElapsed.ToString());
+
+
 //         // Debug for standard events
 //         ar = AnalyticsEvent.LevelComplete("diary_lock");
 //         Debug.Log("LCFinish = " + ar.ToString() + diaryControl.secondsElapsed.ToString());
